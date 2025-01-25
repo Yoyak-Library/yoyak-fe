a37448f58cb9aad8d27f09ba0141fb94445dfbaf
minju0236 <minju0236@gmail.com>
2025-01-25 20:18:41 +0900

    ✨: 로그인, 회원가입 페이지 작성
    Body: 화면 구성 및 일부 js 작성, 서버 연결 미완.
    리액트 이미지 요약러리 이미지로 수정.
    기본 폴더 추가.
    
    Feat: ✨: 로그인, 회원가입 페이지 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/login';
+import SignUp from './pages/login/signup';
 
 function App() {
   return (
     <Router>
       <Routes>
-        
+        {/*로그인*/}
+        <Route path="/" element={<Login />} />
+        {/*회원가입*/}
+        <Route path="/signup" element={<SignUp />}/>
       </Routes>
     </Router>
   );
